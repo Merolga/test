67abf14d52b11a57605e385b745a2f48f030de84
Olga <oolgaa75@mail.ru>
2020-05-03 08:13:29 +0300

    This is first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use sctrict';
+
+let persone = {
+    name: "Olga",
+    age: "28",
+    isMarried: true
+}
+
+console.log("Name is " + persone['name']);
+console.log("Age: " + persone.age);
+console.log("Married: " + persone['isMarried']);
+
+let number = 4;
+    number = +4;
+
+console.log(number);
+
+
